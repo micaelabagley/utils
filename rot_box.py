5135d299029853d71f86933946108c74371f63db
Micaela <micaela.bagley@gmail.com>
2015-03-20 18:14:01 -0500

    included half-finished functions to transform a box into a 'Path' and use it to make a mask

--- rot_box.py
+++ rot_box.py
@@ -33,3 +33,25 @@ def get_region_box(xx, yy):
     path = Path(tbox, closed=True)
     return path
 
+
+def get_region_box(xx, yy):
+    '''Transform a box into a 'Path' '''
+    # get rotation angle in radians
+    theta = get_theta(xx, yy)
+
+    box = np.array([[xx[0],yy[0]], [xx[1],yy[1]], [xx[2],yy[2]],
+                    [xx[3],yy[3]], [xx[0],yy[0]]])
+
+    path = Path(box, closed=True)
+    return path
+
+
+def make_masks():
+    ''' '''
+    paths = []
+    for reg in r:
+        paths.append(get_region_box())
+
+    mask = np.array([True if path.contains_point([x,y]) else False for x,y in np.ndindex(shape)])
+
+    im[np.where(mask)] = 0.0
